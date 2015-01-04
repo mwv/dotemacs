489f7e55b181ea55008d1d8dc8b92a2dd7066859
Maarten Versteegh <maartenversteegh@gmail.com>
2015-01-04 17:29:29 +0100

    more verbose default tester for python

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -1,14 +1,33 @@
 ;; PYTHON
 (elpy-enable)
 (setq elpy-rpc-python-command "/usr/bin/python2")
-(elpy-set-test-runner 'elpy-test-pytest-runner)
+
+(defun elpy-test-pytest-verbose-runner (top file module test)
+  "Test the project using the py.test test runner and print verbose test results."
+  (interactive (elpy-test-at-point))
+  (cond
+   (test
+    (let ((test-list (split-string test "\\.")))
+      (elpy-test-run top
+                     "py.test" "-vv" (mapconcat #'identity
+                                               (cons file test-list)
+                                               "::"))))
+   (module
+    (elpy-test-run top
+                   "py.test" "-vv" file))
+   (t
+    (elpy-test-run top
+                   "py.test" "-vv"))))
+(put 'elpy-test-pytest-verbose-runner 'elpy-test-runner-p t)
+(elpy-set-test-runner 'elpy-test-pytest-verbose-runner)
+
 (setq python-check-command "/usr/bin/pyflakes")
 (add-hook 'python-mode-hook
-	  (lambda ()
-	    (define-key python-mode-map (kbd "C-c C-v") nil)))
+      (lambda ()
+        (define-key python-mode-map (kbd "C-c C-v") nil)))
 (add-hook 'python-mode-hook
-	  (lambda ()
-	    (define-key python-mode-map (kbd "C-c C-v") 'elpy-check)))
+      (lambda ()
+        (define-key python-mode-map (kbd "C-c C-v") 'elpy-check)))
 (elpy-use-ipython)
 
 ;; until cython-mode works
